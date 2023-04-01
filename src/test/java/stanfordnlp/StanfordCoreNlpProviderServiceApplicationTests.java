52f3e2081a4d2846987f61f8110f9de05078bcfb
laura <laura.erb1@gmx.de>
2023-04-01 17:06:23 +0200

    added spring boot skeleton

---
+++ src/test/java/stanfordnlp/StanfordCoreNlpProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package stanfordnlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanfordCoreNlpProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
