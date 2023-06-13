595ed56410a025a5566bf47aef5f12e9748b356a
laura <laura.erb1@gmx.de>
2023-06-13 14:01:00 +0200

    deleted ObjectToDtoConverter.java and added HealthCheckController

---
+++ src/main/java/stanfordnlp/HealthCheckController.java
@@ -0,0 +1,15 @@
+package stanfordnlp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        // no further checks needed for now
+        return ResponseEntity.ok("Microservice is healthy");
+    }
+}
