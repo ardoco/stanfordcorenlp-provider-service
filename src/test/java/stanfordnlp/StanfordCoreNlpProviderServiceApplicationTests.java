867177462e78989719b0ab1fb20154cb0e5d4dac
laura <laura.erb1@gmx.de>
2023-07-09 18:04:34 +0200

    update tests

--- src/test/java/stanfordnlp/StanfordCoreNlpProviderServiceApplicationTests.java
+++ src/test/java/stanfordnlp/StanfordCoreNlpProviderServiceApplicationTests.java
@@ -1,8 +1,11 @@
 package stanfordnlp;
 
 import io.github.ardoco.textproviderjson.converter.JsonConverter;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,22 +25,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 		webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 class StanfordCoreNlpProviderServiceApplicationTests {
+	Logger logger = LoggerFactory.getLogger(StanfordCoreNlpProviderServiceApplicationTests.class);
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
 	void testGetAnnotatedText() throws Exception {
-		// registration
-		String requestBody = "{\"username\": \"user1\", \"password\": \"password1\"}";
-		mvc.perform(post("/registration")
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(requestBody))
-				.andExpect(status().isOk())
-				.andExpect(content().string("User created successfully"));
+		String username = System.getenv("USERNAME");
+		String password = System.getenv("PASSWORD");
+		if(username == null || password == null) {
+			logger.warn("Environment variables USERNAME and PASSWORD are not set. Skipping test.");
+			return;
+		}
 
 		HttpHeaders headers = new HttpHeaders();
-		headers.setBasicAuth("user1", "password1");
+		headers.setBasicAuth(username, password);
 		mvc.perform(get("/stanfordnlp?text=This is Marie.\n")
 						.contentType(MediaType.APPLICATION_JSON)
 						.headers(headers))
@@ -52,28 +55,41 @@ class StanfordCoreNlpProviderServiceApplicationTests {
 				.andExpect(status().isUnauthorized());
 	}
 
-	@Test
-	void testUnsecuredRequest() throws Exception {
-		mvc.perform(get("/health")
-						.contentType(MediaType.TEXT_PLAIN))
-				.andExpect(status().isOk());
-	}
 
 	@Test
 	void testRegistration() throws Exception {
+		String username = System.getenv("USERNAME");
+		String password = System.getenv("PASSWORD");
+		if(username == null || password == null) {
+			logger.warn("Environment variables USERNAME and PASSWORD are not set. Skipping test.");
+			return;
+		}
+		// Test if admin can register a user
+		HttpHeaders adminHeaders = new HttpHeaders();
+		adminHeaders.setBasicAuth(username, password);
 		String requestBody = "{\"username\": \"user1\", \"password\": \"password1\"}";
 		mvc.perform(post("/registration")
 						.contentType(MediaType.APPLICATION_JSON)
-						.content(requestBody))
+						.content(requestBody)
+						.headers(adminHeaders))
 				.andExpect(status().isOk())
 				.andExpect(content().string("User created successfully"));
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.setBasicAuth("user1", "password1");
+		// Test if user can access the service
+		HttpHeaders userHeaders = new HttpHeaders();
+		userHeaders.setBasicAuth("user1", "password1");
 		mvc.perform(get("/stanfordnlp?text=This is Marie.\n")
 						.contentType(MediaType.APPLICATION_JSON)
-						.headers(headers))
+						.headers(userHeaders))
 				.andExpect(status().isOk());
+
+		// Test if user can't register a user
+		String userRequestBody = "{\"username\": \"user2\", \"password\": \"password2\"}";
+		mvc.perform(post("/registration")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(requestBody)
+						.headers(userHeaders))
+				.andExpect(status().isForbidden());
 	}
 
 
