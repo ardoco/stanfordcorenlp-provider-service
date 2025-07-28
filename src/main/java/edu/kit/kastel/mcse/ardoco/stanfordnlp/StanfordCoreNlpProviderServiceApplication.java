0ad56cbaeb9cee76bb2f7ae046cfd2a280956b39
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-07-28 16:45:08 +0200

    fix rest

--- src/main/java/edu/kit/kastel/mcse/ardoco/stanfordnlp/StanfordCoreNlpProviderServiceApplication.java
+++ src/main/java/edu/kit/kastel/mcse/ardoco/stanfordnlp/StanfordCoreNlpProviderServiceApplication.java
@@ -1,9 +1,13 @@
-/* Licensed under MIT 2023. */
+/* Licensed under MIT 2023-2025. */
 package edu.kit.kastel.mcse.ardoco.stanfordnlp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "ArDoCo: Stanford CoreNLP Provider Service", description = "Provides an interface to the Stanford CoreNLP library for text processing."))
 @SpringBootApplication
 public class StanfordCoreNlpProviderServiceApplication {
 
