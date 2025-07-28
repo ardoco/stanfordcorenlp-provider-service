1c415f52cf479a7ed0ab7969f38d45ba29ed4f64
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-07-28 16:58:35 +0200

    Update security

--- src/main/java/edu/kit/kastel/mcse/ardoco/stanfordnlp/StanfordCoreNlpProviderServiceApplication.java
+++ src/main/java/edu/kit/kastel/mcse/ardoco/stanfordnlp/StanfordCoreNlpProviderServiceApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@OpenAPIDefinition(info = @Info(title = "ArDoCo: Stanford CoreNLP Provider Service", description = "Provides an interface to the Stanford CoreNLP library for text processing."))
+@OpenAPIDefinition(info = @Info(title = "ArDoCo: Stanford CoreNLP Provider Service", description = "Provides an interface to the Stanford CoreNLP library for text processing."), security = @SecurityRequirement(name = "basicAuth"))
+@SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic")
 @SpringBootApplication
 public class StanfordCoreNlpProviderServiceApplication {
 
